c304721431dd48d446db5738c92002cc5a1a6c3c
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2023-01-16 21:22:13 +0200

    Edited add-questions template

--- src/app/add-questions/add-questions.component.ts
+++ src/app/add-questions/add-questions.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import { form } from '../interfaces/form';
-import { Form, FormControl, FormGroup, UntypedFormGroup, Validators } from '@angular/forms';
+import { Form, FormControl, FormGroup, UntypedFormArray, UntypedFormGroup, Validators, FormBuilder, FormArray, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { QuestionArrayNamesService } from '../services/question-array-names.service';
 @Component({
@@ -8,27 +8,80 @@ import { QuestionArrayNamesService } from '../services/question-array-names.serv
   templateUrl: './add-questions.component.html',
   styleUrls: ['./add-questions.component.scss']
 })
-export class AddQuestionsComponent{
+export class AddQuestionsComponent implements OnChanges,OnInit{
   courseOptions: any = [];
   questionOptions: any [] = [];
   questionType: any [] = [];
   questionDifficulty: any [] = [];
   myForm: form = {} as form;
-  constructor(private router: Router, private questionsArray: QuestionArrayNamesService) {
+  limit: number = 0 ;
+  selectedIndexMCQ: number;
+  selectedIndexTF: number;
+  constructor(private router: Router, private questionsArray: QuestionArrayNamesService, private fb: FormBuilder ) {
     this.courseOptions = this.questionsArray.courseOptions;
     this.questionOptions = this.questionsArray.questionOptions;
     this.questionType = this.questionsArray.questionType;
     this.questionDifficulty = this.questionsArray.questionDifficulty;
   }
-  addQuestion = new UntypedFormGroup({
+  ngOnInit(): void {
+    
+  }
+  ngOnChanges() {
+    this.addQuestion.get('questionFormat').valueChanges.subscribe(val =>{
+      if(this.addQuestion.controls['questionFormat'].value != 'MCQ'){
+        this.addQuestion.controls['questionAnswer'].clearValidators();
+      }else if (this.addQuestion.controls['questionFormat'].value == 'MCQ'){
+        this.questionOptionsMCQ.controls['question'].clearValidators();
+      }
+    })
+  }
+  addQuestion = this.fb.group({
     courseName: new FormControl('', [Validators.required]),
     questionFormat: new FormControl('', [Validators.required]),
     questionType: new FormControl('', [Validators.required]),
     questionDifficulty: new FormControl('', [Validators.required]),
-    questionTitle: new FormControl('', Validators.required),
-    questionAnswer: new FormControl('', [Validators.required]),
+    questionTitle: new FormControl('', [Validators.required]),
+    questionAnswer: new FormControl('',[Validators.required]),
+    questionOptionsMCQ: this.fb.array([]),
   })
+  // addQuestion = new UntypedFormGroup({
+  //   courseName: new FormControl('', [Validators.required]),
+  //   questionFormat: new FormControl('', [Validators.required]),
+  //   questionType: new FormControl('', [Validators.required]),
+  //   questionDifficulty: new FormControl('', [Validators.required]),
+  //   questionTitle: new FormControl('', Validators.required),
+  //   questionAnswer: new FormControl('', [Validators.required]),
+  //   // questionOptionsMCQ: new FormControl([],[Validators.required]),
+  //   questionOptionsMCQ: new UntypedFormArray([])
+  // })
+  // adminForm = this.fb.group({
+  //   question: new FormControl('', [Validators.required]),
+  //   Answer: new FormControl(''),
+  // });
+
+  get questionOptionsMCQ() {
+    return this.addQuestion.get("questionOptionsMCQ") as FormArray;
+  }
+  addNewOption() {
+      const adminForm = this.fb.group({
+        question: new FormControl('', [Validators.required]),
+        answer: new FormControl(false, [Validators.required]),
+      });
+      this.questionOptionsMCQ.push(adminForm);
+      this.limit++;
+  }
+  changeSelectionCheckBoxMCQ(event, index) {
+    this.selectedIndexMCQ = event.target.checked ? index : undefined;
+  }
+  changeSelectionCheckBoxTF(event, index) {
+    this.selectedIndexTF = event.target.checked ? index : undefined;
+  }
   print() {
+    // this.addQuestion['questionOptionsMCQ'].push(this.adminForm);
+    // this.addQuestion['questionOptionsMCQ'].push(this.adminForm.value);
+    // this.admins.push(this.adminForm);
     console.log(this.addQuestion.value);
+    // console.log(this.addQuestion.controls['questionOptionsMCQ'].controls['question'])
+    // console.log(this.adminForm.value);
   }
 }
