bcd10bad2721fd237f03195ce9144404c7c9ea92
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2023-01-17 02:18:23 +0200

    Fixed bug: formGroup data is saved when chaning questionFormat

--- src/app/add-questions/add-questions.component.ts
+++ src/app/add-questions/add-questions.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
+import { Component, OnChanges, OnInit} from '@angular/core';
 import { form } from '../interfaces/form';
-import { Form, FormControl, FormGroup, UntypedFormArray, UntypedFormGroup, Validators, FormBuilder, FormArray, AbstractControl } from '@angular/forms';
+import { FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
 import { QuestionArrayNamesService } from '../services/question-array-names.service';
 @Component({
@@ -18,70 +18,65 @@ export class AddQuestionsComponent implements OnChanges,OnInit{
   selectedIndexMCQ: number;
   selectedIndexTF: number;
   constructor(private router: Router, private questionsArray: QuestionArrayNamesService, private fb: FormBuilder ) {
+    // getting data from QuestionArrayNamesService
     this.courseOptions = this.questionsArray.courseOptions;
     this.questionOptions = this.questionsArray.questionOptions;
     this.questionType = this.questionsArray.questionType;
     this.questionDifficulty = this.questionsArray.questionDifficulty;
   }
   ngOnInit(): void {
-    
-  }
-  ngOnChanges() {
+    // clearing data when changing questionFormat
     this.addQuestion.get('questionFormat').valueChanges.subscribe(val =>{
       if(this.addQuestion.controls['questionFormat'].value != 'MCQ'){
-        this.addQuestion.controls['questionAnswer'].clearValidators();
+        this.addQuestion.get('questionAnswer').setValidators(Validators.required);
+        this.addQuestion.get('questionOptionsMCQ').clearValidators();
+
       }else if (this.addQuestion.controls['questionFormat'].value == 'MCQ'){
-        this.questionOptionsMCQ.controls['question'].clearValidators();
+        this.addQuestion.get('questionAnswer').clearValidators();
+        this.addQuestion.get('questionOptionsMCQ').setValidators(Validators.required);
+        this.addQuestion.get('questionAnswer').setValue('');
       }
     })
   }
+  ngOnChanges() {
+  }
+  //FormGroup
   addQuestion = this.fb.group({
     courseName: new FormControl('', [Validators.required]),
     questionFormat: new FormControl('', [Validators.required]),
     questionType: new FormControl('', [Validators.required]),
     questionDifficulty: new FormControl('', [Validators.required]),
     questionTitle: new FormControl('', [Validators.required]),
-    questionAnswer: new FormControl('',[Validators.required]),
+    questionAnswer: new FormControl(''),
     questionOptionsMCQ: this.fb.array([]),
   })
-  // addQuestion = new UntypedFormGroup({
-  //   courseName: new FormControl('', [Validators.required]),
-  //   questionFormat: new FormControl('', [Validators.required]),
-  //   questionType: new FormControl('', [Validators.required]),
-  //   questionDifficulty: new FormControl('', [Validators.required]),
-  //   questionTitle: new FormControl('', Validators.required),
-  //   questionAnswer: new FormControl('', [Validators.required]),
-  //   // questionOptionsMCQ: new FormControl([],[Validators.required]),
-  //   questionOptionsMCQ: new UntypedFormArray([])
-  // })
-  // adminForm = this.fb.group({
-  //   question: new FormControl('', [Validators.required]),
-  //   Answer: new FormControl(''),
-  // });
-
   get questionOptionsMCQ() {
     return this.addQuestion.get("questionOptionsMCQ") as FormArray;
   }
+  // Nesting formgroup
   addNewOption() {
-      const adminForm = this.fb.group({
-        question: new FormControl('', [Validators.required]),
-        answer: new FormControl(false, [Validators.required]),
+      const newOption = this.fb.group({
+        question: new FormControl('',[Validators.required]),
+        answer: new FormControl(false),
       });
-      this.questionOptionsMCQ.push(adminForm);
+      this.questionOptionsMCQ.push(newOption);
       this.limit++;
   }
+  // Function to make checkbox be selected once
   changeSelectionCheckBoxMCQ(event, index) {
     this.selectedIndexMCQ = event.target.checked ? index : undefined;
   }
+  // Function to make checkbox be selected once
   changeSelectionCheckBoxTF(event, index) {
     this.selectedIndexTF = event.target.checked ? index : undefined;
   }
   print() {
-    // this.addQuestion['questionOptionsMCQ'].push(this.adminForm);
-    // this.addQuestion['questionOptionsMCQ'].push(this.adminForm.value);
-    // this.admins.push(this.adminForm);
-    console.log(this.addQuestion.value);
-    // console.log(this.addQuestion.controls['questionOptionsMCQ'].controls['question'])
-    // console.log(this.adminForm.value);
+    // resetting questionOptionsMCQ array if coming from other question format to MCQ
+    if(this.addQuestion.controls['questionFormat'].value == 'MCQ') {
+      console.log(this.addQuestion.value);
+    }else {
+      this.addQuestion.controls['questionOptionsMCQ'].reset();
+      console.log(this.addQuestion.value);
+    }
   }
 }
